c79d10f836ef2282f79173ab86c2fac31594431d
Lerot <alsrltt@nate.com>
2017-12-16 14:23:17 +0900

    view

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -5,7 +5,7 @@ $(document).ready( function () {
 
     $.ajax({
         type: 'GET',
-        url: 'http://localhost:3000/users/tweet/friends',
+        url: 'http://192.168.137.130:3000/users/tweet/friends',
         dataType: 'application/json',
         headers: {
             'Content-Type': 'application/json',
